eb2c7996cbaf29a6591d0e9de300349e2befa231
wizie88 <uyieric@ymail.com>
2025-03-13 22:49:42 -0400

    new correction

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
 terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "backend-rg"
-#     storage_account_name = "progetsa"
-#     container_name       = "terraform-state"
-#     key                  = "project.tfstate"
-#     subscription_id      = "a806834c-3019-42c1-867a-d95f6041277b"
- # }
+  backend "azurerm" {
+    # resource_group_name  = "backend-rg"
+    # storage_account_name = "progetsa"
+    # container_name       = "project-container"
+    # key                  = "project.tfstate"
+    # subscription_id      = "a806834c-3019-42c1-867a-d95f6041277b"
+ }
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
